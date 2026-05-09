323a208c494063869ec96b06b5200dad0ca01dbe
MichsSS-G <mich.gribakin.fml@yandex.ru>
2026-05-10 02:44:01 +0500

    refactor: made UserResponseDto immutable

--- user-service/src/main/java/com/cf/user_service/dto/UserResponseDto.java
+++ user-service/src/main/java/com/cf/user_service/dto/UserResponseDto.java
@@ -2,31 +2,15 @@ package com.cf.user_service.dto;
 
 public class UserResponseDto {
 
-    private Long id;
-    private String name;
-    private String surname;
-    private String email;
+    private final Long id;
+    private final String name;
+    private final String surname;
+    private final String email;
 
     public UserResponseDto(Long id, String name, String surname, String email) {
         this.id = id;
         this.name = name;
         this.surname = surname;
-        this.email = email;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 
